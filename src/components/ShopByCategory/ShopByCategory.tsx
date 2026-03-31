dc87a7d79bc0d83d0a10ba45cc9e6922b39d59cf
Vasya <rusnakvasa3@gmail.com>
2026-03-31 19:30:31 +0300

    fix: final router and vite config for gh-pages

--- src/components/ShopByCategory/ShopByCategory.tsx
+++ src/components/ShopByCategory/ShopByCategory.tsx
@@ -6,46 +6,54 @@ import productsData from '/public/api/products.json';
 export const ShopByCategory: FC = () => {
   const baseUrl = import.meta.env.BASE_URL;
 
-  const tablet = productsData.filter(
+  // Рахуємо кількість моделей для кожної категорії
+  const tabletCount = productsData.filter(
     (product: any) => product.category === 'tablets',
   ).length;
-  const phone = productsData.filter(
+  const phoneCount = productsData.filter(
     (product: any) => product.category === 'phones',
   ).length;
-  const accessories = productsData.filter(
+  const accessoriesCount = productsData.filter(
     (product: any) => product.category === 'accessories',
   ).length;
 
   return (
-    <section>
-      <div className="shopByCategory">
-        <h2 className="shopByCategory__h1">Shop by Category</h2>
-        <div className="shopByCategory__items">
-          <Link to="/phones" className="shopByCategory__items__item">
-            {/* Прибрали /public/ і додали baseUrl */}
-            <img src={`${baseUrl}/img/icons/Phones.png`} alt="Phones" />
-            <span className="shopByCategory__items__item__title">Phones</span>
-            <span className="shopByCategory__items__item__models">
-              {phone} models
-            </span>
-          </Link>
-          <Link to="/tablets" className="shopByCategory__items__item">
-            <img src={`${baseUrl}/img/icons/Tablets.png`} alt="Tablets" />
-            <span className="shopByCategory__items__item__title">Tablets</span>
-            <span className="shopByCategory__items__item__models">
-              {tablet} models
-            </span>
-          </Link>
-          <Link to="/accessories" className="shopByCategory__items__item">
-            <img src={`${baseUrl}/img/icons/Accesoirs.png`} alt="Accessories" />
-            <span className="shopByCategory__items__item__title">
-              Accessories
-            </span>
-            <span className="shopByCategory__items__item__models">
-              {accessories} models
-            </span>
-          </Link>
-        </div>
+    <section className="shopByCategory">
+      <h2 className="shopByCategory__h1">Shop by category</h2>
+
+      <div className="shopByCategory__items">
+        {/* КАТЕГОРІЯ: PHONES */}
+        <Link to="/phones" className="shopByCategory__items__item">
+          <div className="shopByCategory__items__item__image-container">
+            <img src={`${baseUrl}img/category-phones.png`} alt="Phones" />
+          </div>
+          <span className="shopByCategory__items__item__title">Mobile phones</span>
+          <span className="shopByCategory__items__item__models">
+            {phoneCount} models
+          </span>
+        </Link>
+
+        {/* КАТЕГОРІЯ: TABLETS */}
+        <Link to="/tablets" className="shopByCategory__items__item">
+          <div className="shopByCategory__items__item__image-container">
+            <img src={`${baseUrl}img/category-tablets.png`} alt="Tablets" />
+          </div>
+          <span className="shopByCategory__items__item__title">Tablets</span>
+          <span className="shopByCategory__items__item__models">
+            {tabletCount} models
+          </span>
+        </Link>
+
+        {/* КАТЕГОРІЯ: ACCESSORIES */}
+        <Link to="/accessories" className="shopByCategory__items__item">
+          <div className="shopByCategory__items__item__image-container">
+            <img src={`${baseUrl}img/category-accessories.png`} alt="Accessories" />
+          </div>
+          <span className="shopByCategory__items__item__title">Accessories</span>
+          <span className="shopByCategory__items__item__models">
+            {accessoriesCount} models
+          </span>
+        </Link>
       </div>
     </section>
   );
